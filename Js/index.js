c44c6d3d264118bb35fe7ecd382f6ad9010ca713
LJ.Agudelo <leidyjohanna64@gmail.com>
2024-06-10 15:18:26 -0500

    se crea calculadora de huevitos

---
+++ Js/index.js
@@ -0,0 +1,10 @@
+const precio = 300000;
+let cantidad = 0;
+let valorTotal = 0;
+
+
+precioSpan = document.querySelector(".precio_inicial");
+precioSpan.innerHTML = precio;
+
+document.querySelector(".cantidad").innerHTML=cantidad;
+document.querySelector(".valorTotal").innerHTML=valorTotal;
